239a3d1d849b8e9ba9fa9b528758619cb20ad96d
12345678zxcvb <1207022628@qq.com>
2023-12-27 15:30:54 +0800

    Save data to memory ,And then go to the userlist interface

--- src/main/java/com/situ/springboot/controller/UserController.java
+++ src/main/java/com/situ/springboot/controller/UserController.java
@@ -5,6 +5,7 @@ import com.situ.springboot.pojo.User;
 import com.situ.springboot.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,15 @@ public class UserController {
     @Autowired
     private UserService userService;
     @RequestMapping("/selectAll")
-    @ResponseBody
+    //@ResponseBody//return json
 
-    public List<User> selectAll() {
+    public String selectAll(Model model) {
 
         System.out.println("UserController.selectAll");
-        return userService.selectAll();
+        List<User>list = userService.selectAll();
+        //store data to memory
+        model.addAttribute("list",list);
+        //转发到user——list界面
+        return "user_list";
     }
 }
